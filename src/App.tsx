07b5b8b04dcee47b91f0ac8ea44df0e4cb0cf1b9
VinaySehwag14 <vinaysehwag14@gmail.com>
2024-12-04 17:03:43 +0530

    setted up basic starting app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hey There , We are starting new project</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
